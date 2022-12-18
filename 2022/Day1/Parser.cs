d927fb37530589fd9615d952fc48d20f4b916103
Lumikrynd <WhatIsEmail?>
2022-12-18 21:08:31 +0100

    actually did day 1... why not

---
+++ 2022/Day1/Parser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1;
+
+internal class Parser : IDisposable
+{
+	public static List<List<int>> ParseInput(string input)
+	{
+		using var parser = new Parser(input);
+
+		return parser.ParseInput();
+	}
+
+	readonly StringReader Stream;
+
+	private Parser(string input)
+	{
+		Stream = new StringReader(input);
+	}
+
+	public void Dispose()
+	{
+		Stream.Dispose();
+	}
+
+	private List<List<int>> ParseInput()
+	{
+		var elves = new List<List<int>>();
+
+		while(Stream.Peek() != -1)
+		{
+			var elf = new List<int>();
+
+			while(true)
+			{
+				var line = Stream.ReadLine();
+				if(string.IsNullOrEmpty(line))
+					break;
+
+				elf.Add(int.Parse(line));
+			}
+
+			elves.Add(elf);
+		}
+
+		return elves;
+	}
+}
--- 2022/Day1/Program.cs
+++ 2022/Day1/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace Day1
+﻿namespace Day1;
+
+internal partial class Program
 {
-	internal class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
-		{
-			Console.WriteLine("Hello, World!");
-		}
+		Part1(Example);
+
+		Console.WriteLine();
+		Console.WriteLine();
+
+		Part1(Input);
+
+		Console.WriteLine();
+		Console.WriteLine();
+
+		Part2(Example);
+
+		Console.WriteLine();
+		Console.WriteLine();
+
+		Part2(Input);
+	}
+
+	private static void Part1(string input)
+	{
+		var elfs = Parser.ParseInput(input);
+
+		var maxTotal = elfs.Max(s => s.Sum());
+
+		Console.Write($"Sum: {maxTotal}");
+	}
+
+	private static void Part2(string input)
+	{
+		var elfs = Parser.ParseInput(input);
+
+		var ordered = elfs.OrderByDescending(elf => elf.Sum());
+
+		var sum = ordered.Take(3).Sum(elf => elf.Sum());
+
+		Console.Write($"Sum: {sum}");
 	}
 }
