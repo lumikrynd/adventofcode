e29be96d5e3fb994c44ef04d84a5a891d0ead109
Lumikrynd <WhatIsEmail?>
2023-01-27 16:28:22 +0100

    a little rewriting  work on day 2

--- 2022/Day02/Game.cs
+++ 2022/Day02/Game.cs
@@ -4,28 +4,65 @@ internal class Game
 {
 	public Move Player { get; init; }
 	public Move Opponent { get; init; }
+	public Result Result { get; init; }
 
 	public Game(Move player, Move opponent)
 	{
 		Player = player;
 		Opponent = opponent;
+		Result = CalculateResult(player, opponent);
+	}
+
+	private Result CalculateResult(Move player, Move opponent)
+	{
+		if(player == opponent)
+			return Result.Draw;
+
+		if(player == CalculateMove(Result.Win, opponent))
+			return Result.Win;
+
+		if(player == CalculateMove(Result.Loose, opponent))
+			return Result.Loose;
+
+		throw new Exception();
 	}
 
 	public Game(Result result, Move opponent)
 	{
 		Opponent = opponent;
+		Result= result;
+		Player = CalculateMove(result, opponent);
+	}
 
-		Player = (Move)(((int)Opponent + 3 + (int)result) % 3);
+	private static Move CalculateMove(Result result, Move opponent)
+	{
+		int move = (int)opponent + (int)result;
+		move %= 3;
+		return (Move)move;
 	}
 
 	public int Score()
 	{
-		var result = 0;
-		result += (((int)Opponent + 1) % 3) == (int)Player ? 6 : 0;
-		result += Opponent == Player ? 3 : 0;
-		result += (int)Player + 1;
+		var score = 0;
+		score += ShapeScore();
+		score += ResultScore();
+		return score;
+	}
 
-		return result;
+	private int ShapeScore()
+	{
+		return (int)Player + 1;
+	}
+
+	private int ResultScore()
+	{
+		return Result switch
+		{
+			Result.Draw => 3,
+			Result.Win => 6,
+			Result.Loose => 0,
+			_ => throw new NotImplementedException(),
+		};
 	}
 
 	public override string ToString()
@@ -43,7 +80,7 @@ internal enum Move
 
 internal enum Result
 {
-	Loose = -1, //X
 	Draw = 0, //Y
 	Win = 1, //Z
+	Loose = 2, //X
 }
