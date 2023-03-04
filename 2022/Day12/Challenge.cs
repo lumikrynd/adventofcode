eb7d3b6ebc39ba44125f624b2951c90e7e03fe6f
Lumikrynd <WhatIsEmail?>
2023-03-04 21:57:19 +0100

    Day 12 some hacky fixes to get second star...

--- 2022/Day12/Challenge.cs
+++ 2022/Day12/Challenge.cs
@@ -1,6 +1,7 @@
 ﻿using Day12.Maps;
 using Day12.Pathfinding;
 using FluentAssertions;
+using Helpers;
 using NUnit.Framework;
 
 namespace Day12;
@@ -27,8 +28,53 @@ internal partial class Challenge
 	private int Part1(IEnumerable<string> textMap)
 	{
 		var map = MapParser.ParseMap(textMap.ToArray());
+		map.Start = map.S;
+		map.FoundEnd = coord => coord.Equals(map.E);
+		map.StepPossible = NormalTraverselStepChecker(map);
+
 		var path = BreathFirstSearch.CalculatePath(map);
 		Console.WriteLine(path.Count);
 		return path.Count;
+	}
+
+	private Func<Coordinate, Coordinate, bool> NormalTraverselStepChecker(Map map)
+	{
+		return (from, to) =>
+		{
+			var maxHeigh = map.GetHeightAt(from) + 1;
+			return map.GetHeightAt(to) <= maxHeigh;
+		};
+	}
+
+	[Test]
+	public void Part2_Example()
+	{
+		var result = Part2(ExampleInput);
+		result.Should().Be(29);
+	}
+
+	[Test]
+	public void Part2_MainPuzzle()
+	{
+		var result = Part2(PuzzleInput);
+		result.Should().Be(363);
+	}
+
+	private int Part2(IEnumerable<string> textMap)
+	{
+		var map = MapParser.ParseMap(textMap.ToArray());
+		map.Start = map.E;
+		map.FoundEnd = coord => map.GetHeightAt(coord) <= 0;
+		map.StepPossible = InvertedTraverselStepChecker(map);
+
+		var path = BreathFirstSearch.CalculatePath(map);
+		Console.WriteLine(path.Count);
+		return path.Count;
+	}
+
+	private Func<Coordinate, Coordinate, bool> InvertedTraverselStepChecker(Map map)
+	{
+		var temp = NormalTraverselStepChecker(map);
+		return (from, to) => temp(to, from);
 	}
 }
--- 2022/Day12/Map/Map.cs
+++ 2022/Day12/Map/Map.cs
@@ -4,23 +4,23 @@ namespace Day12.Maps;
 
 internal class Map
 {
-	public Map(int[,] fields, Coordinate start, Coordinate goal)
+	public Map(int[,] fields, Coordinate s, Coordinate e)
 	{
 		Fields = fields;
-		Start = start;
-		Goal = goal;	
+		S = s;
+		E = e;	
 	}
 
 	public int[,] Fields { get; }
-	public Coordinate Start { get; }
-	public Coordinate Goal { get; }
+	public Coordinate S { get; }
+	public Coordinate E { get; }
 
-	internal List<Coordinate> PossibleSteps(Coordinate coordinate)
+	internal List<Coordinate> PossibleSteps(Coordinate current)
 	{
 		List<Coordinate> steps = new();
 
-		int x = coordinate.X;
-		int y = coordinate.Y;
+		int x = current.X;
+		int y = current.Y;
 
 		var X = Fields.GetLength(0) - 1;
 		var Y = Fields.GetLength(1) - 1;
@@ -30,11 +30,13 @@ internal class Map
 		if (y > 0) steps.Add(new(x, y - 1));
 		if (y < Y) steps.Add(new(x, y + 1));
 
-		var maxHeigh = Fields[x, y] + 1;
-
-		var result = steps.Where(c => Fields[c.X, c.Y] <= maxHeigh).ToList();
+		var result = steps.Where(c => StepPossible(current, c)).ToList();
 		return result;
 	}
 
-	internal bool FoundEnd(Coordinate coordinate) => coordinate.Equals(Goal);
+	public int GetHeightAt(Coordinate coordinate) => Fields[coordinate.X, coordinate.Y];
+	public Func<Coordinate, Coordinate, bool> StepPossible { get; set; } = (_, _) => throw new NotImplementedException();
+
+	public Func<Coordinate, bool> FoundEnd { get; set; } = _ => throw new NotImplementedException();
+	public Coordinate Start { get; set; } = new(0, 0);
 }
