a64e6c98e549ee73a2e7efae5c32e2e77fc1b261
Lumikrynd <WhatIsEmail?>
2023-03-14 18:46:11 +0100

    Day 15 part 1 solved... slow

--- 2022/Day15/Challenge.cs
+++ 2022/Day15/Challenge.cs
@@ -27,26 +27,35 @@ internal class Challenge
 	{
 		var sensorResponses = SensorResponseParser.ParseInput(input);
 		var map = MapCreater.CreateMap(sensorResponses);
-		var result = map.CountRowCowerage(row);
-		Console.WriteLine($"Covered lines: {result}");
+		var coveredArea = map.CountRowCowerage(row);
+		Console.WriteLine($"Covered lines: {coveredArea}");
+
+		var beaconCount = sensorResponses
+			.Select(s => s.Beacon)
+			.Distinct()
+			.Count(b => b.Y == row);
+		Console.WriteLine($"Beacons on line: {beaconCount}");
+
+		var result = coveredArea - beaconCount;
+		Console.WriteLine($"result: {result}");
 		return result;
 	}
 
 	[Test]
 	public void Part2_Example()
 	{
-		var result = Part2(ExampleInput);
-		//result.Should().Be(???);
+		var result = Part2(ExampleInput, 0, 20);
+		result.Should().Be(56000011);
 	}
 
 	[Test]
 	public void Part2_MainPuzzle()
 	{
-		var result = Part2(PuzzleInput);
+		var result = Part2(PuzzleInput, 0, 4000000);
 		//result.Should().Be(???);
 	}
 
-	private int Part2(IEnumerable<string> textMap)
+	private long Part2(IEnumerable<string> textMap, int min, int max)
 	{
 		throw new NotImplementedException();
 	}
--- 2022/Day15/Model/Map.cs
+++ 2022/Day15/Model/Map.cs
@@ -1,4 +1,5 @@
-﻿using Helpers;
+﻿using System.Text;
+using Helpers;
 
 namespace Day15.Model;
 
@@ -24,5 +25,20 @@ internal class Map : IMap
 	{
 		var relativeRow = row + Displacement.Ys;
 		return RootNode.CountRowCowerage(relativeRow);
+	}
+
+	public string GetMapArea(Coordinate lower, Coordinate upper)
+	{
+		var sb = new StringBuilder();
+		for(int y = lower.Y; y <= upper.Y; y++)
+		{
+			for(int x = lower.X; x <= upper.X; x++)
+			{
+				char c = HasContent(new(x, y)) ? '#' : '.';
+				sb.Append(c);
+			}
+			sb.AppendLine();
+		}
+		return sb.ToString();
 	}
 }
