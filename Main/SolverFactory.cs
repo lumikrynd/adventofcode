9bd97f634cfb981b021c23f22d09bb9186148090
Lumikrynd <WhatIsEmail?>
2026-04-12 16:24:33 +0200

    Fix conflicting dll names in Main project
    Turns out including multiple projects with the exact same name causes
    problems.
    From what I can see it is because the dll's are all put into the same
    bin folder, so including 2 of the same name makes the overwrite each other.
    
    I fixed the problem by adding some Directory.Build.props files which
    prepend the year to all dll names.
    
    -------------------------------------------------------------------
    At least this answered an old question for me with why it can cause
    problems if 2 nuget packages depend on the same project but very
    different versions of it.
    
    ...
    Also, it really is a mess in that bin folder that it both end up with
    all the testing libraries, and that some of the included projects are
    executables on their own... Really is a weird project trying to refactor
    my old code-base O_o

--- Main/SolverFactory.cs
+++ Main/SolverFactory.cs
@@ -11,12 +11,27 @@ internal static class SolverFactory
 		[MaybeNullWhen(false)] out Func<IEnumerable<string>, ISolver> factory)
 	{
 		switch (year) {
-			case 2023:
-				return Y2023(day, out factory);
+			case 2022: return Y2022(day, out factory);
+			case 2023: return Y2023(day, out factory);
 			default:
 				factory = null;
 				return false;
 		}
+	}
+
+	private static bool Y2022(
+		int day,
+		[MaybeNullWhen(false)] out Func<IEnumerable<string>, ISolver> factory) {
+		switch (day) {
+			case 1:
+				factory = x => new Y2022.Day01.Challenge(x);
+				break;
+			default:
+				factory = null;
+				break;
+		}
+
+		return factory is not null;
 	}
 
 	private static bool Y2023(
